2613041380872ac73a990a427c29af9836718de4
merazzak <info.merazzak@gmail.com>
2025-06-19 00:01:31 +0600

    add modal in place feedback button

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -65,6 +65,7 @@ const Navbar = () => {
               </a>
             ))}
             <button
+              onClick={() => document.getElementById("my_modal_3").showModal()}
               className={`border border-[#007A88] px-4 py-1 text-sm rounded-full transition ${
                 isScrolled
                   ? "text-[#007A88] border-[#007A88]"
@@ -146,6 +147,19 @@ const Navbar = () => {
           </button>
         </div>
       </Container>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        </div>
+      </dialog>
+      ;
     </header>
   );
 };
